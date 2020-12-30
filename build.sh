ff5dc6c76c9fb187f7436f79cc809e214c4109ed
Brent Shambaugh <brent.shambaugh@gmail.com>
2020-12-30 11:35:12 -0600

    Change build.sh

--- build.sh
+++ build.sh
@@ -4,13 +4,13 @@ make
 for f in *.out; do
     valgrind -v --log-file="${f%.*}"_memory.txt ./$f
 done
-mkdir ardunio
+mkdir floatToUint8HexStringArdunio
 cd src
 for f in *c; do 
-    cp "$f" "../ardunio/${f%.c}.cpp"
+    cp "$f" "../floatToUint8HexStringArdunio/${f%.c}.cpp"
 done
-cp *.h ../ardunio/
+cp *.h ../floatToUint8HexStringArdunio/
 cd ..
-rm ardunio/main.cpp
-cp src/library_properties/library.properties ardunio/
-zip -r floatToUint8HexStringArdunio.zip ardunio/
+rm floatToUint8HexStringArdunio/main.cpp
+cp src/library_properties/library.properties floatToUint8HexStringArdunio/
+zip -r floatToUint8HexStringArdunio.zip floatToUint8HexStringArdunio/
